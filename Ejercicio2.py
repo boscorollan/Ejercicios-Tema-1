efe8076bd3a339754d2a8c232eb99fd9d0a615fa
boscorollan <bosco.rollanagudo@usp.ceu.es>
2025-09-28 17:18:43 +0000

    3.0

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,7 +3,7 @@ def ahorro(cant_inicial, cant_mensual, meses):
     if total >= 5000:
         print(f"Felcidades has ahorrado {total}")
     else:
-        print(f"Has ahorrado {total} euros en {meses} meses")
+        print(f"Has ahorrado {total} euros en {meses} meses.")
 cantidad_inicial = float(input("Ingrese la cantidad inicial: "))
 cantidad_mensual = float(input("Ingrese la cantidad mensual a ahorrar: "))
 meses = int(input("Ingrese la cantidad de meses que va a ahorrar: "))
